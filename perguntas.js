f895cb7227b53da4640cb6113539a8ba4d490485
andrew henrique salata lopes <144800185+walter7white@users.noreply.github.com>
2024-10-07 08:32:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'questão de matematica',
+    '1 + 1 = ?',
+    '11, parabéms, voce é mais inteligente q 90% da sociedade'
+)
+
+criaCartao(
+    'Quimica',
+    'Como se fabrica sabonete apartir da decomposição quimica da lipoaspiração das pessoas',
+    'sla pesquisa aekkk'
+)
+
+criaCartao(
+    'progamação',
+    'como se cria um pegasus spyware do governo e rouba dados de milhares de cidadoes',
+    'eu nao sei, mas o governo sabe.'
+)
+
+criaCartao(
+    'Pergunta seria.',
+    'Os lados de um triangulo de vertices A, B e C medem AB = 3CM , BC= 7CM e CA = 8CM. A circunferencia inscrita no triangulo tangencia o lado AB no Ponto N e o lado CA no ponto K. Então, o comprimento do segmento NK em cm é',
+    'A resposta é 2.'
+)
